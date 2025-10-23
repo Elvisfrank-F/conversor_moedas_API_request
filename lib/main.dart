34357a1eecb0e8d4847d4c8748acfd8e46925d95
Elvisfrank-F <gordotesla@gmail.com>
2025-10-23 08:59:04 -0300

    Corrigindo erro de não buscar dados na API ao baixar o APK, e otimizando o código

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:http/http.dart' as http;
 import 'dart:async';
 import 'dart:convert';
 
+
 void main(){
 
   runApp(MyApp());
@@ -28,227 +29,139 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
 
-  TextEditingController _controllerBR = new TextEditingController();
-
-  TextEditingController _controllerUSD = new TextEditingController();
-  
-
+ TextEditingController _controllerRS = new TextEditingController();
+ TextEditingController _controllerUSD = new TextEditingController();
  TextEditingController _controllerEURO = new TextEditingController();
-  
-
-
+ 
   double dolar = 0;
   double euro = 0;
+  double real = 0;
 
-  bool isNumber(String s){
-    try{
-      double x = double.parse(s);
-      return true;
-    }
-    catch(e){
-      return false;
-    }
-  }
+  void _changedReal(String s){
 
-  void _changedBR(String a){
-
-    if(_controllerBR.text.isEmpty){
-      _controllerUSD.text = "";
-      _controllerEURO.text = "";
-    }
-
-    if(isNumber(a)) {
-       double real = double.parse(a);
+    real = double.tryParse(s) ?? 0;
     _controllerUSD.text = (real/dolar).toStringAsFixed(2);
-    _controllerEURO.text= (real/euro).toStringAsFixed(2);
-    }
-      else {
-    String a = _controllerBR.text;
-    _controllerBR.text ="";
-     for(int i=0;i<a.length-1;i++){
-      _controllerBR.text += a[i];
-     }
-  }
-  
+    _controllerEURO.text = (real/euro).toStringAsFixed(2);
+
   }
 
-  void _changedUSD(String a){
+  void _changedUSD(String s){
 
-      if(_controllerUSD.text.isEmpty){
-      _controllerBR.text = "";
-      _controllerEURO.text = "";
-    }
+    double dolarInsert = double.tryParse(s) ?? 0;
+    _controllerRS.text = (dolar * dolarInsert).toStringAsFixed(2);
+    _controllerEURO.text = ( (dolar * dolarInsert)/ euro).toStringAsFixed(2);
 
-    if(isNumber(a)) {
-       double real = double.parse(a);
-    _controllerBR.text = (real * dolar).toStringAsFixed(2);
-    _controllerEURO.text = ((real * dolar) / euro).toStringAsFixed(2);
-  
-    }
-
-      else {
-    String a = _controllerUSD.text;
-    _controllerUSD.text ="";
-     for(int i=0;i<a.length-1;i++){
-      _controllerUSD.text += a[i];
-     }
-  }
   }
 
-    void _changedEURO(String a){
+   void _changedEURO(String s){
 
-        if(_controllerEURO.text.isEmpty){
-      _controllerUSD.text = "";
-      _controllerBR.text = "";
-    }
+    double euroInsert = double.tryParse(s) ?? 0;
+    _controllerUSD.text = ( (euro * euroInsert) / dolar).toStringAsFixed(2);
+    _controllerRS.text = ( (euro * euroInsert)).toStringAsFixed(2);
 
-      if(isNumber(a)){
-  double real = double.parse(a);
-    _controllerBR.text = (real * euro).toStringAsFixed(2);
-    _controllerUSD.text = ( ((real * euro) / dolar)).toStringAsFixed(2);
   }
-  else {
-    String a = _controllerEURO.text;
-    _controllerEURO.text ="";
-     for(int i=0;i<a.length-1;i++){
-      _controllerEURO.text += a[i];
-     }
-  }
-  }
-  
+
+
+
+
+ 
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.amber,
-        title: Text("Conversor",
-        style: TextStyle(
-          fontSize: 25,
-          color: Colors.white
-        )),
+        title: Text("\$Conversor\$",
+        style: TextStyle(fontSize: 25, color: Colors.white)),
         centerTitle: true,
+        
       ),
       backgroundColor: Colors.black,
-      body: FutureBuilder(
-        future: getData(),
-        builder:(context, snapshot){
+      body: FutureBuilder(future: getData(), 
+      builder: (context, snapshot){
+        
+        switch(snapshot.connectionState) {
+            case (ConnectionState.none):
+            case (ConnectionState.waiting):
+            return WaitingPage();
 
-          switch(snapshot.connectionState){
-
-            case ConnectionState.none:
-            case ConnectionState.waiting:
-
-            return Center(child: Text("Carregando dados...",
-            style: TextStyle(color: Colors.amber, fontSize: 20)));
-            
             default: 
             if(snapshot.hasError){
-
-               return Center(child: Text("Não foi possivel carregar os dados 404 no found :(",
-            style: TextStyle(color: Colors.amber, fontSize: 20)));
-           
+              return ErrorPage();
             }
             else {
-              dolar = snapshot.data!["results"]["currencies"]["USD"]["buy"];
-              euro = snapshot.data!["results"]["currencies"]["EUR"]["buy"];
-
-         return SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Center(
-              child: Column(
-                children: [
-                  Icon(Icons.monetization_on, size: 150, color: Colors.amber),
-                  TextField(
-                    controller: _controllerBR,
-                    onChanged: _changedBR,
-                    style: TextStyle(
-                      color: Colors.amber,
-                      fontSize: 20,
-                    ),
-                    decoration: InputDecoration(
-                      label: Text("Reais",
-                      style: TextStyle(
-                        color: Colors.amber
-                      )),
-                      prefix: Text("R\$: ",
-                      style: TextStyle(
-                        color: Colors.white
-                      )),
-                      fillColor: Colors.amber,
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10)
-                      )
-                    )
-                  ),
-                  Divider(),
-            
-                  TextField(
-                    controller: _controllerUSD,
-                    onChanged: _changedUSD,
-                    style: TextStyle(
-                      color: Colors.amber,
-                      fontSize: 20,
-                    ),
-                    decoration: InputDecoration(
-                      label: Text("Dolas",
-                      style: TextStyle(
-                        color: Colors.amber
-                      )),
-                      prefix: Text("\$: ",
-                      style: TextStyle(
-                        color: Colors.white
-                      )),
-                      fillColor: Colors.amber,
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10)
-                      )
-                    )
-                  ),
-            
-                  Divider(),
-            
-                  TextField(
-                    controller: _controllerEURO,
-                    onChanged: _changedEURO,
-                    style: TextStyle(
-                      color: Colors.amber,
-                      fontSize: 20,
-                    ),
-                    decoration: InputDecoration(
-                      label: Text("Euro",
-                      style: TextStyle(
-                        color: Colors.amber
-                      )),
-                      prefix: Text("E\$: ",
-                      style: TextStyle(
-                        color: Colors.white
-                      )),
-                      fillColor: Colors.amber,
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10)
-                      )
-                    )
-                  ),
-                ],
-              )
-            ),
-          ),
-            
-        );
+              dolar = snapshot.data!["results"]["currencies"]["USD"]["buy"] ?? 0;
+              euro = snapshot.data! ["results"]["currencies"]["EUR"]["buy"] ?? 0;
+              return PageMain();
             }
+
+
         }
-        }
-        
-      )
+      })
     );
   }
 
-  Future<Map> getData() async{
-    final String request = "https://api.hgbrasil.com/finance?format=json&key=7f5863ad";
-    http.Response response = await http.get(Uri.parse(request));
+  Widget ErrorPage(){
+    return Center(child: Text("Error ao carregar os dados", style: TextStyle(color: Colors.amber, fontSize: 20)));
+  }
 
+  Widget WaitingPage(){
+    return Center(child: Text("Carregando dados..", style: TextStyle(color: Colors.amber, fontSize: 20)));
+  }
+
+  Widget PageMain(){
+    return Center(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+            
+                Icon(Icons.monetization_on, size: 150, color: Colors.amber),
+                SizedBox(height: 30),
+                TextFieldPerson(Changed: _changedReal,prefix: "R\$: ", label: "Reais", controller: _controllerRS),
+                SizedBox(height: 10),
+                TextFieldPerson(Changed: _changedUSD, prefix: "\$USD: ",label: "Dólares", controller: _controllerUSD),
+                SizedBox(height: 10),
+                TextFieldPerson(Changed: _changedEURO, prefix: "\$EUR: ",label: "EURO", controller: _controllerEURO)
+                
+              ],
+            ),
+          ),
+        ),
+      );
+  }
+
+  Future<Map> getData() async {
+    const request = "https://api.hgbrasil.com/finance?format=json&key=7f5863ad";
+    http.Response response = await http.get(Uri.parse(request));
     return jsonDecode(response.body);
   }
+
+Widget TextFieldPerson({required ValueChanged<String> Changed,required String label,  required String prefix, required TextEditingController controller}){
+  return TextField(
+     keyboardType: TextInputType.number,
+    controller: controller,
+    onChanged: Changed,
+    onTap: () {
+     controller.text = "";
+    },
+    
+    style: TextStyle(
+      color: Colors.amber, fontSize: 20,
+    ),
+    decoration: InputDecoration(
+      prefix: Text(prefix,
+      style: TextStyle(color: Colors.white)),
+      label: Text(label,
+      style: TextStyle(color: Colors.amber)),
+      fillColor: Colors.amber,
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(10)
+      )
+    )
+  );
 }
+
+}
+
